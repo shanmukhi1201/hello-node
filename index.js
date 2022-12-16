30767f2854ab69968343fa98404695cb3ac0c6d0
shanmukhi <pavanivenkatashanmukhi345@gmail.com>
2022-12-16 09:19:03 +0530

    implemented streams

--- index.js
+++ index.js
@@ -1,5 +1,46 @@
-const hello = 'Hello Node.js!'
-const returnHello = () => {
-  console.log(hello)
-}
-returnHello()
+// const hello = 'Hello Node.js!'
+// const returnHello = () => {
+//   console.log(hello)
+// }
+// returnHello()
+// const fs = require("fs");
+// fs.writeFile(
+//   "sample.txt",
+//   "Hello World. Welcome to Node.js File System module.",
+//   (err) => {
+//     if (err) throw err;
+//     else {
+//       console.log("File created!");
+//     }
+//   }
+// );
+// fs.readFile("sample.txt", (err, data) => {
+//   if (err) throw err;
+//   console.log(data.toString());
+// });
+
+// fs.appendFile("sample.txt", " This is my updated content", (err) => {
+//   if (err) throw err;
+//   console.log("File updated!");
+// });
+
+// fs.rename("sample.txt", "test.txt", (err) => {
+//   if (err) throw err;
+//   console.log("File name updated!");
+// });
+
+// fs.unlink("test.txt", (err) => {
+//   if (err) throw err;
+//   console.log("File test.txt deleted successfully!");
+// });
+const http = require("http");
+const fs = require("fs");
+const server = http.createServer((req, res) => {
+  const stream = fs.createReadStream("sample.txt");
+  stream.pipe(res);
+  // fs.readFile("sample.txt", (err, data) => {
+  //   res.end(data);
+  // });
+});
+ 
+server.listen(3000);
